e42448d40474a2e34f749acd1443bc347b4cc927
Evans <denziedon@gmail.com>
2024-02-25 18:16:22 +0300

    Created a Student

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,10 +1,13 @@
 package com.example.springboot;
 
+import com.example.springboot.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -16,8 +19,16 @@ public class Application {
 	}
 
 	@GetMapping("/hello")
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Somebody",
+						LocalDate.of(2000, Month.JULY,28),
+						"somebody@gmail.com",
+						22
+				)
+		);
 	}
 
 }
